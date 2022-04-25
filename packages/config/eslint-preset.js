ff6ae96f31b4ec5c8e41562a236dc55c8ed3c376
Mateusz Flisikowski <contact@mflisikowski.dev>
2022-04-25 18:45:52 +0200

    eslint, tsconfig update

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,5 +1,18 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "next",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
   settings: {
     next: {
       rootDir: ["apps/*/", "packages/*/"],
@@ -9,4 +22,5 @@ module.exports = {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
+  plugins: ["@typescript-eslint"],
 };
